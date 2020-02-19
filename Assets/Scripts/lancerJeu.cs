a180f3dde183a8a64927d9498a1e9cd20c254361
NassimMhb <mhb.nassim@gmail.com>
2020-02-19 19:09:53 +0100

    add modifs

---
+++ Assets/Scripts/lancerJeu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lancerJeu : MonoBehaviour
+{
+    public void GoToMainMenu()
+    {
+        Application.LoadLevel("FirstScene");
+    }
+}
